b525757194dc1b5b826de442588c0d419c7f0368
Baltasar Aroso <baltasar@xx.network>
2022-12-20 15:04:36 +0000

    fix decimal issue on economics table values to calculate avg return

--- src/pages/simple/utils/index.tsx
+++ src/pages/simple/utils/index.tsx
@@ -3,9 +3,9 @@ import { BN, BN_MILLION } from '@polkadot/util';
 import { Economics } from '../../../schemas/economics.schema';
 
 export const getStakedReturn = (economics: Economics): number => {
-  const totalStaked = new BN(stripNonDigits(economics?.staked.toString().slice(15)));
-  const tmStaked = new BN(stripNonDigits(economics?.tmStaked.toString().slice(15)));
-  const stakeableSupply = new BN(stripNonDigits(economics?.stakeableSupply.toString().slice(15)));
+  const totalStaked = new BN(stripNonDigits(economics?.staked.toString().slice(0,-3)));
+  const tmStaked = new BN(stripNonDigits(economics?.tmStaked.toString().slice(0,-3)));
+  const stakeableSupply = new BN(stripNonDigits(economics?.stakeableSupply.toString().slice(0,-3)));
   const inflationRate = new BN(stripNonDigits(economics?.inflationRate)).toNumber();
   const stakedFraction =
     totalStaked.mul(BN_MILLION).div(stakeableSupply).toNumber() / BN_MILLION.toNumber();
